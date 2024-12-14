465e6bb2ea312f71bda31c9cc70c300fbfb899fc
himansu8 <himansusekharsahu50@gmail.com>
2024-12-14 23:07:02 +0530

    private route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -7,17 +7,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar';
 import { Toaster } from 'react-hot-toast'
 import SummaryPage from './components/Summary';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
 
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext)
+
+    if (!user) {
+      return <Navigate to='/login' />
+    }
+    return children;
+  }
 
 
   return (
     <>
 <Routes>
   
-  <Route path="/" element={<><NavbarComponent/><Home/></>} />
-  <Route path="/dashboard" element={<><NavbarComponent/><SummaryPage/></>} />
+  <Route path="/" element={<><ProtectedRoute><NavbarComponent/><Home/></ProtectedRoute></>} />
+  <Route path="/dashboard" element={<><ProtectedRoute><NavbarComponent/><SummaryPage/></ProtectedRoute></>} />
   <Route path='/login' element={<Login   />} />
   <Route path='/signup' element={<Signup/>} />
 </Routes>
